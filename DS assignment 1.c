854c6d2d52fab07f1286d6be955fbaddedbf0a7d
Chamarthi-nagamani <68267259+Chamarthi-nagamani@users.noreply.github.com>
2020-12-27 10:06:32 +0530

    Add files via upload

---
+++ DS assignment 1.c
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+for(i=1; i<=n; i++)
+    for(j=1; j<=n;j++)
+        print('hi')
+        
+time complexity == O(n2)
+
+for(i=1; i<=n; i*=3)
+    for(j=1; j<=n;j++)
+        print('hi')
+
+time complexity == O(n)
